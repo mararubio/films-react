ab9f1f8dc515e9c71746e07ee3b352e99e21b725
mararubio <mararubio70@alumnos.cei.es>
2025-07-25 13:40:42 +0200

    errores

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react" // Importar contexto global y estado.
+import { useState, useContext, Navigate } from "react" // Importar contexto global y estado.
 import { useNavigate } from "react-router-dom" // Para redirigir a "/"
 import Context from "./Contexto.jsx" // Importar el contexto
 import "./index.css" // Importar el css
@@ -9,16 +9,18 @@ export default function Login() {
     let { setToken } = useContext(Context) // Necesitamos el token
     let [inputUser, setInputUser] = useState("") // El valor del input de user
     let [inputPassword, setInputPassword] = useState("") // El valor del input del password
-    let [error,setError] = useState(null) // Mensaje de error para el usuario
-
-    return <>
+    let [error, setError] = useState(null) // Mensaje de error para el usuario
+    if (token == "") {
+        return <Navigate to="/" />
+    } else {
+        return <>
             <section className="Section-login">
                 <img src="./logo-filmtap.webp" alt="Logo de FilmTap" className="Logo" />
                 <h1> Film<span style={{ color: "#455bd8" }}>Tap</span>:<br />Reseñas sin filtros
                 </h1>
                 <p>Entra, comenta y puntúa tus pelis favoritas </p>
             </section>
-            <form onSubmit={ event => { // Formulario con 3 campos (user + password, submit)
+            <form onSubmit={event => { // Formulario con 3 campos (user + password, submit)
                 event.preventDefault() // Lo enviamos con fetch
                 setError(null)
                 if (inputUser.trim() != "" && inputPassword.trim() != "") { // Validar que no estén vacíos.
@@ -32,7 +34,7 @@ export default function Login() {
                             "Content-type": "application/json"
                         }
                     })
-                        .then(res => { 
+                        .then(res => {
                             if (res.status == 200) {
                                 return res.json()
                                     .then(({ token }) => { // token
@@ -40,7 +42,7 @@ export default function Login() {
                                         navigate("/") // Redirigir a "/"
                                     })
                             } else if (res.status == 401) { // Aquí puede pasar el error (mensajes personalizados)
-                                setError("Usuario o contraseña incorrectos") 
+                                setError("Usuario o contraseña incorrectos")
                             } else if (res.status == 403) {
                                 setError("No tienes acceso")
 
@@ -49,7 +51,7 @@ export default function Login() {
                             }
                         })
                 } else {
-                    setError("Rellena todos los campos del formulario para iniciar sesión")   
+                    setError("Rellena todos los campos del formulario para iniciar sesión")
                 }
             }}>
                 <input type="text" placeholder="Usuario" value={inputUser} onChange={event => setInputUser(event.target.value)} />
@@ -57,11 +59,12 @@ export default function Login() {
                 <input type="submit" className="Login-btn"
                     value="iniciar sesión" />
             </form>
-            {error && <p className="Error">{error}</p>} 
+            {error && <p className="Error">{error}</p>}
             {/* Si no es null, es visible el Error */}
 
-        <footer className="Footer">
-            <p className="Footer-copy">&copy; 2025 Maria Rubio</p>
-        </footer>
-    </>
+            <footer className="Footer">
+                <p className="Footer-copy">&copy; 2025 Maria Rubio</p>
+            </footer>
+        </>
+    }
 }
